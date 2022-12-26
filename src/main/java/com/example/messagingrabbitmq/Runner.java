062c8d9b036bd4b2f625d33a68c8c7d2083f1d72
Denis <Barkas074@yandex.ru>
2022-12-26 06:25:46 +0500

    JMS

--- src/main/java/com/example/messagingrabbitmq/Runner.java
+++ src/main/java/com/example/messagingrabbitmq/Runner.java
@@ -1,5 +1,6 @@
 package com.example.messagingrabbitmq;
 
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -9,19 +10,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class Runner implements CommandLineRunner {
 
-	private final RabbitTemplate rabbitTemplate;
-	private final Receiver receiver;
+    private final RabbitTemplate rabbitTemplate;
+    private final Receiver receiver;
 
-	public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
-		this.receiver = receiver;
-		this.rabbitTemplate = rabbitTemplate;
-	}
+    public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
+        this.receiver = receiver;
+        this.rabbitTemplate = rabbitTemplate;
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Sending message...");
-		rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
-		receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String message;
+        do {
+            message = sc.nextLine();
+            if (message.equals("\\q")) break;
+            rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.exchangeName, "", message);
+            receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+        } while (true);
+    }
 
 }
